d492bdb9455942bf3573207ef79d1e2095d5d2ae
Kevin Schanz <kschanz@imobile3.com>
2020-05-19 18:23:29 -0400

    Game files - clean and import data

---
+++ stats/data.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import os
+import glob
+
+import pandas as pd
+
+game_files = glob.glob(os.path.join(os.getcwd(), 'games', '*.EVE'))
+game_files.sort()
+
+game_frames = []
+# Append game frames
+for game_file in game_files:
+    game_frame = pd.read_csv(game_file, names=['type', 'multi2', 'multi3', 'multi4', 'multi5', 'multi6', 'event'])
+    game_frames.append(game_frame)
+
+# Concatenate DataFrames
+games = pd.concat(game_frames)
+
+# Clean values
+games.loc[games['multi5'] == '??', ['multi5']] = ''
+
+# Extract identifiers
+identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
+# Forward fill identifiers
+identifiers = identifiers.fillna(method='ffill')
+identifiers.columns = ['game_id', 'year']
+
+# Concatenate identifier columns
+games = pd.concat([games, identifiers], sort=False, axis=1)
+# Fill NaN (Not a Number) values
+games = games.fillna(' ')
+games.loc[:, 'type'] = pd.Categorical(games.loc[:, 'type'])
+
+# Print DataFrame
+print(games.head())
